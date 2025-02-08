1c9bb2f7a391152da19fdea3d81265786d19021e
Gobinath.v <gobinathvb24@gmail.com>
2025-02-08 11:25:17 +0530

    define the model and initial commit

---
+++ model/knex.js
@@ -0,0 +1,8 @@
+const knex = require("knex");
+// checking the env running
+const knexFile = require("../knexfile")[process.env.NODE_ENV || "development"];
+
+module.exports = (settings) => {
+  if (!settings) settings = knexFile;
+  return new knex(settings);
+};
